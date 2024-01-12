48bf19f48b349d5441734e2f0c05258cd4895f43
Igor Shaposhnikov <114292165+IgorShaposhnikov@users.noreply.github.com>
2024-01-12 17:41:49 +0500

    Added abstract for Calandar Object and Media Object (#6)
    * Delete useless files
    
    * Add timer and tests for it
    
    * Added abstract for Calendar and Media objects

---
+++ HealthFirst/HealthFirst/Abstracts/CalendarObject/CalendarObjectRepeatInfo.cs
@@ -0,0 +1,32 @@
+﻿namespace HealthFirst.Core.CalendarObject
+{
+    public readonly struct CalendarObjectRepeatInfo
+    {
+        /// <summary>
+        /// Type of repeats list Weekly, Daily...
+        /// </summary>
+        public readonly CalendarObjectRepeat Type;
+        /// <summary>
+        /// Every x days, weeks, months, years...
+        /// Every = 2 (days) -> Mon, Wen, Fri, Sat...
+        /// </summary>
+        public readonly uint Every;
+        /// <summary>
+        /// Starts from next avaiable data.
+        /// </summary>
+        public readonly DateTime Starts;
+        /// <summary>
+        /// Create at this time.
+        /// </summary>
+        public readonly DateTime CreateAt;
+        /// <summary>
+        /// Time Zone (GMT +-...)
+        /// </summary>
+        public readonly TimeZoneInfo TimeZone;
+        /// <summary>
+        /// Selected Day of Week.
+        /// For Type - Weekly only!
+        /// </summary>
+        public readonly DayOfWeek[] SelectedDayOfWeek;
+    }
+}
