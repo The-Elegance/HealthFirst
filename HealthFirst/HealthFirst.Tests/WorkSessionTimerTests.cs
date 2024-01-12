73a98ff12c6b1fd7e848a5798e78ea1462e9f9e4
Hel Johansson <igor.shaposhnikov@urfu.me>
2024-01-12 11:15:44 +0500

    Add timer and tests for it

---
+++ HealthFirst/HealthFirst.Tests/WorkSessionTimerTests.cs
@@ -0,0 +1,66 @@
+﻿using HealthFirst.Core.Timer;
+using NUnit.Framework;
+
+namespace HealthFirst.Tests
+{
+    [TestFixture]
+    internal class WorkSessionTimerTests
+    {
+        [SetUp] public void SetUp() 
+        { }
+
+        [Test]
+        public void TimerWillBeFinishedWithPausedAndResume() 
+        {
+            var i = 0;
+            var timer = new WorkSessionTimer(TimeSpan.FromSeconds(5));
+
+            timer.Paused += (timerPausedData) =>
+            {
+                i++;
+            };
+
+            timer.Start();
+            Thread.Sleep(2000);
+            timer.Stop();
+
+            timer.Start();
+            Thread.Sleep(2000);
+            timer.Stop();
+
+            timer.Start();
+
+            Assert.AreEqual(2, i);
+        }
+
+        [Test]
+        public void TimerCanBePaused() 
+        {
+            var timer = new WorkSessionTimer(TimeSpan.FromSeconds(3));
+
+            timer.Start();
+            Thread.Sleep((int)TimeSpan.FromSeconds(2).TotalMilliseconds);
+            timer.Stop();
+
+            timer.Paused += (timerPausedData) =>
+            {
+                Assert.AreEqual(1, timerPausedData.RemainingTime);
+            };
+        }
+
+        [Test]
+        public void TimerCanBeFinished() 
+        {
+            var timer = new WorkSessionTimer(TimeSpan.FromSeconds(3));
+
+            timer.Start();
+            Thread.Sleep((int)TimeSpan.FromSeconds(2).TotalMilliseconds);
+            timer.Stop();
+
+            timer.Finished += () =>
+            {
+                Assert.AreEqual(true, true);
+            };
+        }
+    }
+}
