206bc7023c9684d6d724b04e4b9d3d99aab3b574
Hel Johansson <igor.shaposhnikov@urfu.me>
2024-01-17 15:35:59 +0500

    Changed ICalandarObject property name and refactor abstracts

--- HealthFirst/HealthFirst/Abstracts/CalendarObject/CalendarObjectRepeatInfo.cs
+++ HealthFirst/HealthFirst/Abstracts/CalendarObject/CalendarObjectRepeatInfo.cs
@@ -1,32 +1,38 @@
 ﻿namespace HealthFirst.Core.CalendarObject
 {
-    public readonly struct CalendarObjectRepeatInfo
+    public readonly struct CalendarObjectRepeatInfo(
+        CalendarObjectRepeat type,
+        uint every,
+        DateTime starts,
+        DateTime createAt,
+        TimeZoneInfo timeZoneInfo,
+        DayOfWeek[] selectedDayOfWeek = null)
     {
         /// <summary>
         /// Type of repeats list Weekly, Daily...
         /// </summary>
-        public readonly CalendarObjectRepeat Type;
+        public readonly CalendarObjectRepeat Type = type;
         /// <summary>
         /// Every x days, weeks, months, years...
         /// Every = 2 (days) -> Mon, Wen, Fri, Sat...
         /// </summary>
-        public readonly uint Every;
+        public readonly uint Every = every;
         /// <summary>
         /// Starts from next avaiable data.
         /// </summary>
-        public readonly DateTime Starts;
+        public readonly DateTime Starts = starts;
         /// <summary>
         /// Create at this time.
         /// </summary>
-        public readonly DateTime CreateAt;
+        public readonly DateTime CreateAt = createAt;
         /// <summary>
         /// Time Zone (GMT +-...)
         /// </summary>
-        public readonly TimeZoneInfo TimeZone;
+        public readonly TimeZoneInfo TimeZone = timeZoneInfo;
         /// <summary>
         /// Selected Day of Week.
         /// For Type - Weekly only!
         /// </summary>
-        public readonly DayOfWeek[] SelectedDayOfWeek;
+        public readonly DayOfWeek[] SelectedDayOfWeek = selectedDayOfWeek;
     }
 }
